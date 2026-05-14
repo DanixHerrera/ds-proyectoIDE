d157c754b6b52a50be685f1b17f3c223cd7f21a1
txttttt <kdjimeneze@gmail.com>
2026-05-14 13:52:03 -0600

    coso login

--- DesktopAppPrototipo_4/StudentIDE/Views/LoginView.xaml.cs
+++ DesktopAppPrototipo_4/StudentIDE/Views/LoginView.xaml.cs
@@ -9,5 +9,22 @@ namespace StudentIDE.Views
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainView m = new MainView();
+            m.Show();
+            this.Close();
+        }
+
+        private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+
+        private void OlvideContrasena_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
