c5fd8b9ad4c57c92e64218c93329f50effcffb72
Daniel Herrera <166463042+DanixHerrera@users.noreply.github.com>
2026-06-01 19:19:26 -0600

    confirmar

--- DesktopAppPrototipo_4/StudentIDE/obj/Release/net8.0-windows/StudentIDE.AssemblyInfo.cs
+++ DesktopAppPrototipo_4/StudentIDE/obj/Release/net8.0-windows/StudentIDE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentIDE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c12ba97feffc6a79d64a848b04b51c5b7b075be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+800a053fbc11d80e0191ab4d18ce68faada5a6fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentIDE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentIDE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
