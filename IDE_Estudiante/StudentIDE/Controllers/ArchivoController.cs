67f73089d830135274057f53882b559f0eed1876
TXT <167579669+txttttt@users.noreply.github.com>
2026-06-25 03:31:45 +0000

    Update ArchivoController.cs

--- IDE_Estudiante/StudentIDE/Controllers/ArchivoController.cs
+++ IDE_Estudiante/StudentIDE/Controllers/ArchivoController.cs
@@ -1,24 +1,26 @@
 using System.IO;
-
 namespace StudentIDE.Controllers
 {
- 
+
     // Gestiona el guardado del archivo en disco.
- 
+
     public class ArchivoController
     {
 
         //Guarda el contenido del editor en la ruta exacta indicada.
         //retorna true si el guardado fue exitoso, false si ocurrió un error.
-   
+        private FirmaDigitalController FC = new FirmaDigitalController();
+
         public bool Guardar(string rutaCompleta, string contenido)
         {
             try
             {
                 File.WriteAllText(rutaCompleta, contenido, System.Text.Encoding.UTF8);
+                String firma = FC.CrearFirma(contenido);
+                File.WriteAllText(rutaCompleta + ".sig", firma, System.Text.Encoding.UTF8);
                 return true;
             }
-            catch
+            catch (Exception)
             {
                 //el ViewModel maneja el mensaje de error hacia la UI
                 return false;
