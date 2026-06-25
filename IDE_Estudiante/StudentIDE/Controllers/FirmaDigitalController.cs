c333fe47f376a506060094471c168825a80711b1
TXT <167579669+txttttt@users.noreply.github.com>
2026-06-25 03:25:39 +0000

    Iteracion 1 FirmaDigital
    Por favor revisar si funciona

--- IDE_Estudiante/StudentIDE/Controllers/FirmaDigitalController.cs
+++ IDE_Estudiante/StudentIDE/Controllers/FirmaDigitalController.cs
@@ -1,20 +1,35 @@
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
 namespace StudentIDE.Controllers
 {
- 
+
     // Genera y verifica la firma digital SHA-256 al archivo .py creado en el IDE
 
     public class FirmaDigitalController
     {
+        private const string clave =
+            "ClaveSecretaMuyMuySecretaDificilDeAdivinar";
 
-        /*
-         public string GenerarFirma(string contenido) {
-
-
+        public string CrearFirma(string contenido)
+        {
+            using var encriptador = new HMACSHA256(Encoding.UTF8.GetBytes(clave));
+            byte[] resultadoBytes = encriptador.ComputeHash(Encoding.UTF8.GetBytes(contenido));
+            return Convert.ToBase64String(resultadoBytes);
         }
-        */
-        /*
-        public bool VerificarFirma(string contenidoArchivo) {
-            
-        }*/
+
+        // rutacompleta NO incluye la terminacion .sig
+        public bool VerificarFirma(string rutacompleta, string contenido)
+        {
+            string hashResultado = CrearFirma(contenido);
+            try
+            {
+                string firma = File.ReadAllText(rutacompleta + ".sig").Trim();
+                return (String.Equals(firma, hashResultado, StringComparison.Ordinal));
+            } catch (FileNotFoundException) {
+                return false;
+            }
+        }
     }
 }
